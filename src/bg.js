446423fde721f3e978fa987e20fe3df288a68e24
Cretzen <47333723+hellotinh03@users.noreply.github.com>
2021-07-11 13:33:05 +0700

    Update bg.js

--- src/bg.js
+++ src/bg.js
@@ -6,10 +6,10 @@ const minute = 60000;
 let scale = 1;
 body.style.transformOrigin = `top center`;
 
-function shrinktome() {
+function shrinktome_all() {
   if (scale < 0.05) return clearInterval(shrinktome);
   scale *= 0.95;
   body.style.transform = `scale(${scale}, ${scale})`;
 }
 
-setTimeout(() => setInterval(shrinktome, minute/10), 500);
+setTimeout(() => setInterval(shrinktome_all, minute/10), 500);
